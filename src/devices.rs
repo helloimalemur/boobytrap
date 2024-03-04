f1410945c3a9c8cb4eb306d4e2072dc0953bdea2
jamesk <james.k@konnektive.com>
2024-03-04 12:16:40 -0500

    .

--- src/devices.rs
+++ src/devices.rs
@@ -1,5 +1,6 @@
 use crate::tw::EventMonitor;
 use crate::tw::*;
+use std::process::Command;
 
 pub struct USBMon {
     triggered: bool,
@@ -20,5 +21,34 @@ impl USBMon {
 impl EventMonitor for USBMon {
     async fn check(&mut self) {
         println!("check usb: {}", self.triggered);
+        get_usb_devices_physical().await;
     }
+}
+
+async fn get_usb_devices() {
+    let mut result = String::new();
+    let command_str = "cat /proc/bus/input/devices";
+    if let Ok(res) = Command::new("sh").arg("-c").arg(command_str).output() {
+        result = String::from_utf8(res.stdout.to_vec()).unwrap();
+    }
+    println!("{}", result)
+}
+
+async fn get_usb_devices_physical() {
+    let mut devices: Vec<String> = vec![];
+    let mut result = String::new();
+    let command_str = "cat /proc/bus/input/devices | grep 'S:'";
+    if let Ok(res) = Command::new("sh")
+        .arg("-c")
+        .arg(command_str)
+        .output()
+    {
+        result = String::from_utf8(res.stdout.to_vec()).unwrap();
+        result.split('\n').for_each(|r| {
+            if !r.split(' ').last().unwrap().to_string().is_empty() {
+                devices.push(r.split(' ').last().unwrap().to_string())
+            }
+        })
+    }
+    println!("{:#?}", devices)
 }
