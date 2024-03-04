9366700bb5919dd19ec5d972e960909548dbfae1
jamesk <james.k@konnektive.com>
2024-03-04 16:16:07 -0500

    .

--- src/actions.rs
+++ src/actions.rs
@@ -1,7 +1,40 @@
+use std::io::BufRead;
 use std::process::Command;
 
 pub async fn reboot_system() {
     if let Ok(reboot) = Command::new("reboot").output() {
         println!("{:#?}", reboot)
+    }
+}
+
+pub async fn unmount_encrypted_volumes() {
+    if let Ok(output) = Command::new("dmsetup").arg("ls").output() {
+        let encrypted_vols = String::from_utf8(output.stdout.to_vec()).unwrap();
+        for ea in encrypted_vols.lines() {
+            let vol_split: Vec<_> = ea.split_ascii_whitespace().collect();
+            let vol = vol_split.get(0).unwrap().to_string();
+            println!("{}", vol);
+            let command_str = format!("mount | grep {}", vol);
+            println!("{}", command_str);
+            // let res = Command::new("mount")
+            //     .arg("-l")
+            //     .arg("|")
+            //     .arg("grep")
+            //     .arg(vol)
+            //     .output();
+            // println!("{:#?}", res);
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::actions::unmount_encrypted_volumes;
+
+    #[test]
+    fn test_unmount() {
+        let rt = tokio::runtime::Runtime::new();
+        rt.unwrap().block_on(unmount_encrypted_volumes());
     }
 }
