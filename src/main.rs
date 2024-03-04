c965e88c717a65af546cf875bcbfda2647ef1bfb
jamesk <james.k@konnektive.com>
2024-03-04 12:45:22 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,5 @@ mod tw;
 #[tokio::main]
 async fn main() {
     let mut app = AppState::new();
-    // let mut binding = app.monitors.lock();
-    // let app_lock = binding.as_mut().unwrap();
-    //
-    // app_lock.push(Monitors::USBMon(USBMon::new()));
-    // app_lock.push(Monitors::NetMon(NETMon::new()));
-
     app.run().await;
 }
