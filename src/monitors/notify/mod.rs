e5edb91f21f12dd73dcb4fe6cad69ea642365e6b
jkoonts <jkoontsiii@gmail.com>
2024-03-13 15:49:11 -0400

    .

--- src/monitors/notify/mod.rs
+++ src/monitors/notify/mod.rs
@@ -1,9 +1,22 @@
 use std::collections::HashMap;
+use std::process::Command;
 
 pub async fn send_discord(message: &str, settings_map: HashMap<String, String>) -> Result<(), reqwest::Error> {
+    let mut final_message = String::new();
+
+    if let Ok(output) = Command::new("hostnamectl").arg("hostname").output() {
+        if let Ok(hostname) = String::from_utf8(output.stdout) {
+            final_message = format!("{} Hostname: {}", message, hostname);
+        } else {
+            final_message = message.to_string();
+        }
+    } else {
+        final_message = message.to_string();
+    }
+
     let discord_webhook_url = settings_map.get("discord_webhook_url").unwrap();
     let discord_webhook_avatar_name = settings_map.get("discord_webhook_avatar_name").unwrap();
-    discord_webhook_lib::send_discord(discord_webhook_url, message ,discord_webhook_avatar_name).await
+    discord_webhook_lib::send_discord(discord_webhook_url, final_message.as_str(), discord_webhook_avatar_name).await
 }
 
 // pub async fn send_email() {}
