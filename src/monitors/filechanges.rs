4ff3b3e364e5526746ffc875bce9f4407cba844b
jkoonts <jkoontsiii@gmail.com>
2024-03-12 20:01:13 -0400

    .

--- src/monitors/filechanges.rs
+++ src/monitors/filechanges.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
+use std::sync::LockResult;
 use Fasching::create_snapshot;
 use Fasching::hasher::HashType;
-use Fasching::snapshot::Snapshot;
+use Fasching::snapshot::{FileMetadata, Snapshot};
 use crate::monitors::filechanges;
 use crate::monitors::notify::send_discord;
 use crate::tw::EventMonitor;
@@ -44,15 +45,18 @@ impl FileChanges {
 impl EventMonitor for FileChanges {
     async fn check(&mut self) {
         if self.step > 10 {
-            if !compare_snapshots(self.settings_map.clone()) {
+            println!("check fs changes: {}", self.triggered);
+
+            if !compare_snapshots(self, self.settings_map.clone()).await {
                 self.triggered = true
             }
+
             if self.triggered {
                 println!("File Change Alert!");
-                fs_changes_alert(self.settings_map.clone()).await;
+                // fs_changes_alert(, self.settings_map.clone()).await;
                 let _ = send_discord("File Change Alert!!", self.settings_map.clone()).await;
             }
-            println!("check fs changes: {}", self.triggered);
+
             self.step = 0;
         } else {
             self.step += 1;
@@ -79,10 +83,52 @@ fn get_hash_type(settings_map: HashMap<String, String>) -> HashType {
     }
 }
 
-async fn fs_changes_alert(settings_map: HashMap<String, String>) {
-    // reboot_system(settings_map).await;
+async fn compare_snapshots(file_changes: &mut FileChanges, settings_map: HashMap<String, String>) -> bool {
+    let mut success = true;
+    if let Some(last) = file_changes.snapshots.pop() {
+        let current = create_snapshot(last.root_path.as_str(), last.hash_type);
+
+        match last.file_hashes.lock() {
+            Ok(last_lock) => {
+
+                for last_entry in last_lock.iter() {
+
+                    match current.file_hashes.lock() {
+                        Ok(curr_lock) => {
+
+                            match curr_lock.get(last_entry.0) {
+                                Some(new_entry) => {
+                                    if !new_entry.check_sum.eq(&last_entry.1.check_sum) {
+                                        file_changes.triggered = true;
+                                        let message = format!("File Changes: {}, {}", new_entry.path, new_entry.mtime);
+                                        fs_changes_alert(message, settings_map.clone()).await
+                                    } else {
+                                        println!("check sum check passed");
+                                        println!("{}: {}", new_entry.size, new_entry.path);
+                                    }
+                                }
+                                None => {success = false}
+                            }
+
+                        }
+                        Err(_) => {success = false}
+
+                    }
+
+                }
+
+            }
+            Err(_) => {success = false}
+        }
+
+
+        file_changes.snapshots.push(current)
+    }
+
+    println!("TOTAL SNAPSHOTS: {:#?}", file_changes.snapshots.len());
+    success
 }
 
-fn compare_snapshots(settings_map: HashMap<String, String>) -> bool {
-    true
+async fn fs_changes_alert(message: String, settings_map: HashMap<String, String>) {
+    let _ = send_discord(message.as_str(), settings_map).await;
 }
